c75e4dc04a46a5a0e28b37576fcb47b4a896ec29
Alessandro Petitcollin <petitcollin.alessandro@gmail.com>
2024-08-09 19:38:32 +0200

    Started from scratch

---
+++ include/philosophers.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apetitco <apetitco@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/09 19:19:51 by apetitco          #+#    #+#             */
+/*   Updated: 2024/08/09 19:19:51 by apetitco         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_PHILOSOPHERS_H
+#define PHILOSOPHERS_PHILOSOPHERS_H
+
+#include <stdbool.h>
+
+typedef bool	t_error;
+
+# define ERROR 1
+# define NO_ERROR 0
+
+#endif
---
+++ src/philosophers.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apetitco <apetitco@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/09 19:19:59 by apetitco          #+#    #+#             */
+/*   Updated: 2024/08/09 19:19:59 by apetitco         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+//TODO Parsing (pas remplir structure, juste checker validité des arguments)
+//TODO Réfléchir au nombre de mutex à utiliser
+//TODO Faire structure pour les philos et pour le ruleset
+//TODO Que mettre dans la structure de philo ?
+//TODO Inclure le ruleset dans la structure philo
+//TODO Erreurs custom
+
+int	main(int argc, char **argv)
+{
+	if (argc != 5 && argc != 6)
+		return
+}
