c0c1528dd414e594464ff1a5937007d7d2881438
Alessandro Petitcollin <petitcollin.alessandro@gmail.com>
2024-08-13 14:43:44 +0200

    Prepared header

--- include/philosophers.h
+++ include/philosophers.h
@@ -1,8 +1,16 @@
-//
-// Created by apetitco on 8/13/24.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apetitco <apetitco@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/13 14:42:44 by apetitco          #+#    #+#             */
+/*   Updated: 2024/08/13 14:42:44 by apetitco         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#ifndef PHILOSOPHERS_PHILOSOPHERS_H
-#define PHILOSOPHERS_PHILOSOPHERS_H
+#ifndef PHILOSOPHERS_H
+#define PHILOSOPHERS_H
 
-#endif //PHILOSOPHERS_PHILOSOPHERS_H
+#endif
