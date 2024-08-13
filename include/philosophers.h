53fb65da22a22b21743666e2c5eeb73f46c48bbb
Alessandro Petitcollin <petitcollin.alessandro@gmail.com>
2024-08-13 14:57:59 +0200

    Added check_input() function.

--- include/philosophers.h
+++ include/philosophers.h
@@ -13,4 +13,14 @@
 #ifndef PHILOSOPHERS_H
 #define PHILOSOPHERS_H
 
+#include <stdbool.h>
+
+typedef bool t_error;
+
+#define ERROR 1
+#define NO_ERROR 0
+
+//----- INITIALISATION -----//
+t_error	check_input(int argc, char *argv[]);
+
 #endif
--- src/philosophers.c
+++ src/philosophers.c
@@ -12,7 +12,32 @@
 
 #include "philosophers.h"
 
+/// \brief Checks for invalid characters in the passed arguments.
+/// \param argc Number of CLI arguments.
+/// \param argv CLI arguments.
+/// \return ERROR(1) if a non-digit character is encountered, else NO_ERROR(O).
+t_error	check_input(int argc, char *argv[])
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while (argv[i])
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+				return (ERROR);
+			j++;
+		}
+	}
+	return (NO_ERROR);
+}
+
 int	main(int argc, char *argv[])
 {
-
+	if (check_input(argc, argv))
+		return (ERROR);
+	return (NO_ERROR);
 }
