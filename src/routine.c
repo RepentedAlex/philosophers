fb17c799f264753ab507d6d9175c1955811ddc69
Alessandro Petitcollin <petitcollin.alessandro@gmail.com>
2024-08-20 14:14:12 +0200

    It seems to be bare minimum working!

--- src/routine.c
+++ src/routine.c
@@ -49,6 +49,7 @@ void routine(t_philo *philo)
 		{
 			pthread_mutex_lock(&philo->philo_lock);
 			philo->status = replete;
+			philo->ruleset->nb_replete_philos++;
 			pthread_mutex_unlock(&philo->philo_lock);
 			return ;
 		}
@@ -57,7 +58,8 @@ void routine(t_philo *philo)
 		{
 			pthread_mutex_lock(&philo->philo_lock);
 			philo->status = dead;
-			printf("%ld: Philo [%d] has died.\n", get_time(), philo->id);
+			printf("%ld %d died.\n", get_time(), philo->id);
+			philo->ruleset->stop = 1;
 			pthread_mutex_unlock(&philo->philo_lock);
 		}
 		if (first_round == 0)
@@ -81,7 +83,7 @@ void	philo_eat(t_philo *philo)
 	{
 		pthread_mutex_lock(&philo->philo_lock);
 		philo->status = eating;
-		printf("%ld : philo [%d] is eating...\n", get_time(), philo->id);
+		printf("%ld %d is eating\n", get_time(), philo->id);
 		ft_usleep(philo->ruleset->time_to_eat);
 
 		pthread_mutex_unlock(&philo->philo_lock);
@@ -91,14 +93,14 @@ void	philo_eat(t_philo *philo)
 	{
 		pthread_mutex_lock(&philo->philo_lock);
 		philo->status = eating;
-		printf("%ld : philo [%d] is eating...\n", get_time(), philo->id);
+		printf("%ld %d is eating\n", get_time(), philo->id);
 		ft_usleep(philo->ruleset->time_to_eat);
 		pthread_mutex_unlock(&philo->philo_lock);
 		pthread_mutex_unlock(&philo->neighbor[1]->philo_lock);
 	}
 	philo->last_meal = get_time();
 	philo->nb_of_meals++;
-	printf("%ld : philo [%d] meals: %d/%d\n", get_time(),
+	printf("%ld : philo %d meals: %d/%d\n", get_time(),
 		   philo->id, philo->nb_of_meals, philo->ruleset->max_meals);
 	philo_sleep(philo);
 }
@@ -108,7 +110,7 @@ void	philo_sleep(t_philo *philo)
 	if (!philo)
 		return;
 	philo->status = sleeping;
-	printf("%ld : philo [%d] is sleeping...\n", get_time(), philo->id);
+	printf("%ld %d is sleeping\n", get_time(), philo->id);
 	ft_usleep(philo->ruleset->time_to_sleep);
 	philo_think(philo);
 }
@@ -116,5 +118,5 @@ void	philo_sleep(t_philo *philo)
 void	philo_think(t_philo *philo)
 {
 	philo->status =thinking;
-	printf("%ld : philo [%d] is thinking...\n", get_time(), philo->id);
+	printf("%ld %d is thinking\n", get_time(), philo->id);
 }
